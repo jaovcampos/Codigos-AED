8a3a093e6099a3fa92adfa2c26e39f72cd8f5f39
jaovcampos <164241222+jaovcampos@users.noreply.github.com>
2024-03-21 11:23:32 -0300

    att.

--- POO/lista1_1.cpp
+++ POO/lista1_1.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Agenda {
+private:
+    string nome;
+    string telefone;
+public:
+    void setNome(string nome) {
+        this->nome = nome;
+    }
+
+    string getNome() {
+        return nome;
+    }
+
+    void setTelefone(string telefone) {
+        this->telefone = telefone;
+    }
+
+    string getTelefone() {
+        return telefone;
+    }
+
+    void printAgenda() {
+        cout << nome << "," << telefone << "\n";
+    }
+};
+
+int main() {
+    Agenda a;
+    a.setNome("Ana");
+    a.setTelefone("613738238");
+
+    return 0;
+}
