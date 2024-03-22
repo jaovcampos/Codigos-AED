772866ff03a9e99004642aec0f167e3469233a14
jaovcampos <164241222+jaovcampos@users.noreply.github.com>
2024-03-21 22:08:32 -0300

    att.

--- POO/lista1_2.cpp
+++ POO/lista1_2.cpp
@@ -1,1 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Aluno {
+
+private:
+    
+    string nome;
+    int matricula;
+    string curso;
+
+public:
+    
+    Aluno(string _nome, int _matricula, string _curso) : nome(_nome), matricula(_matricula), curso(_curso) {
+        
+    }
+
+    void exibirDados() const {
+        cout << "Nome: " << nome << "\n";
+        cout << "Matricula: " << matricula << "\n";
+        cout << "Curso: " << curso << "\n";
+    }
+
+    string getNome() const { return nome; }
+    int getMatricula() const { return matricula; }
+    string getCurso() const { return curso; }
+};
+
+class GerenciadorAlunos {
+    
+private:
+    vector<Aluno> alunos;
+
+public:
+    void cadastrarAluno(const Aluno& aluno) {
+        
+    }
+
+    void cadastrarNovoAluno() {
+        string nome, curso;
+        int matricula;
+
+        cout << "Informe o nome do aluno: ";
+        getline(cin, nome);
+
+        cout << "Informe a matricula do aluno: ";
+        cin >> matricula;
+
+        cout << "Informe o curso do aluno: ";
+        getline(cin, curso);
+
+        Aluno novoAluno(nome, matricula, curso);
+        cadastrarAluno(novoAluno);
+    }
+
+    void listarAlunos() const {
+        cout << "Lista de Alunos:" << "\n";
+        for (const auto& aluno : alunos) {
+            aluno.exibirDados();
+            cout << "\n";
+        }
+    }
+};
+
+int main() {
+    GerenciadorAlunos gerenciador;
+
+    gerenciador.cadastrarNovoAluno();
+    gerenciador.cadastrarNovoAluno();
+    gerenciador.listarAlunos();
+
+    return 0;
+}
 
