d54656e49ec9f6a084d1f9c8c35408ec9311ee5b
jaovcampos <164241222+jaovcampos@users.noreply.github.com>
2024-03-28 20:14:57 -0300

    Delete POO/lista1_7.cpp

--- POO/lista1_7.cpp
+++
@@ -1,4 +0,0 @@
-Não consegui realizar esse exercício professor. Precisei de bastante ajuda para realizar as outras tarefas,
-peço que explique um pouco mais detalhadamente em suas aulas como realizar as atividades.
-Nosso professor de CPE não foi tão afundo em algumas questões no semestre passado. 
-Fora isso, sua aula tem acrescentado muito! (Peço que não exponha essa mensagem para a sala de aula).
