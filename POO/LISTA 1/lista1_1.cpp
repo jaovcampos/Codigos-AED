2ce83bea2d14f6ad04c426bee226649780619555
jaovcampos <164241222+jaovcampos@users.noreply.github.com>
2024-03-28 20:20:37 -0300

    Add files via upload

---
+++ POO/LISTA 1/lista1_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Agenda {
+private:
+    string nome;
+    string telefone;
+public:
+    void setNome(string nome) {
+        this->nome = nome;
+    }
+
+    string getNome() {
+        return nome;
+    }
+
+    void setTelefone(string telefone) {
+        this->telefone = telefone;
+    }
+
+    string getTelefone() {
+        return telefone;
+    }
+
+    void printAgenda() {
+        cout << nome << "," << telefone << "\n";
+    }
+};
+
+int main() 
+{
+    Agenda a;
+    a.setNome("Ana");
+    a.setTelefone("613738238");
+    a.printAgenda();
+    
+    return 0;
+}
