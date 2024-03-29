083e26ccc81d2023460c33042c32fae0562a0460
jaovcampos <cscjoao@hotmail.com>
2024-03-29 20:37:39 -0300

    Create lista2_4.cpp
    att.

---
+++ POO/LISTA 2/lista2_4.cpp
@@ -0,0 +1,13 @@
+// c) Estado, comportamento e identidade.
+
+// Estado: Refere-se aos atributos ou dados que um objeto contém. É a representação dos valores que descrevem o objeto 
+//em um determinado momento. Por exemplo, em uma classe Carro, o estado pode incluir atributos como marca, modelo, ano,
+//cor, etc.
+
+//Comportamento: Refere-se aos métodos ou funções que um objeto pode executar. Esses métodos definem as ações que um
+//objeto pode realizar e como ele pode interagir com seu estado interno e outros objetos. Por exemplo, em uma classe 
+//Carro, métodos como acelerar(), frear(), ligar(), desligar(), etc., representam o comportamento do carro.
+
+//Identidade: Refere-se à característica única de um objeto que o diferencia de outros objetos. Cada objeto tem sua 
+//própria identidade única, que geralmente é representada por um endereço de memória ou um identificador único. Isso 
+//permite que o programa faça referência e distinga entre diferentes objetos.
