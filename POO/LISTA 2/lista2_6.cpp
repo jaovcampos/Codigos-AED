b680f5d25f9ff3de08e166b28261980d8f51f1b8
jaovcampos <cscjoao@hotmail.com>
2024-03-29 21:00:38 -0300

    att.

---
+++ POO/LISTA 2/lista2_6.cpp
@@ -0,0 +1,32 @@
+//O encapsulamento é um conceito fundamental na programação orientada a objetos (POO) que envolve a combinação de dados
+//(atributos) e comportamentos (métodos) em uma única unidade, chamada de classe. O encapsulamento permite ocultar os 
+//detalhes internos de implementação de uma classe, fornecendo uma interface clara e controlada para interagir com os 
+//objetos dessa classe. Aqui estão algumas razões para usar o encapsulamento em seu programa:
+
+//Ocultar Detalhes de Implementação:
+
+// O encapsulamento permite esconder os detalhes internos de implementação de uma classe, fornecendo apenas a interface
+// pública através da qual os usuários podem interagir com os objetos. Isso facilita a manutenção do código, pois os 
+//detalhes de implementação podem ser modificados sem afetar os usuários da classe.
+
+//Proteger os Dados:
+
+//O encapsulamento permite controlar o acesso aos dados da classe, tornando-os privados ou protegidos e fornecendo 
+//métodos públicos para acessá-los e modificá-los de maneira segura. Isso impede que dados internos sejam manipulados 
+//de forma inadequada por partes externas do código, garantindo consistência e integridade dos dados.
+
+//Promover a Reutilização de Código:
+
+//Ao encapsular dados e comportamentos relacionados em uma classe, você pode reutilizar essa classe em diferentes partes
+//do seu programa ou em diferentes programas. Isso promove a modularidade e facilita a manutenção do código, já que as 
+//mudanças feitas em uma classe encapsulada são isoladas e não afetam outras partes do sistema.
+
+---------------------------------
+|            Classe             |
+---------------------------------
+| - atributoPrivado: Tipo      |
+| # atributoProtegido: Tipo    |
+| + metodoPublico() : void     |
+| - metodoPrivado() : void     |
+| # metodoProtegido() : void   |
+---------------------------------
